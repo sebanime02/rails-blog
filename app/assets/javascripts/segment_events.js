ca35c1d6325a0a8113ee2e9a4b1ed951111c5687
Sebastian Benavides <sebastian.benavides@codescrum.com>
2018-02-06 17:55:29 -0500

    adding customization with  segment_events.js 2

--- app/assets/javascripts/segment_events.js
+++ app/assets/javascripts/segment_events.js
@@ -13,13 +13,23 @@ $(function()
 
     $("#url_home").on('click',function()
       {
-        track_events('Order completed', properties);
+        var event_properties_1 = {revenue: 33};
+
+        var event_properties_2 =
+        {
+            category: 'Account',
+            label: 'Premium',
+            value: 50
+          };
+
+        track_events('Url home adwords', event_properties_1);
+        track_events('Url home analytics',event_properties_2);
+
       });
-
-
 
     function track_events(name, properties)
     {
+      console.log(name+' Launched');
       analytics.track(name,properties);
     }
 
